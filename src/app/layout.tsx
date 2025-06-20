1ea29ec23808fd7e66c27807d4119ea12f6200bc
Jonghwi Kim <54308894+bellhwi@users.noreply.github.com>
2025-06-20 14:38:22 -0700

    Add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { ReactNode } from 'react'
 import FeedbackWidget from '@/components/FeedbackWidget'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'LiftLite',
@@ -12,7 +13,32 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
+      <head>
+        {/* GTM HEAD SCRIPT */}
+        <Script
+          id='gtm-head'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5S4ZJML8');
+            `,
+          }}
+        />
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-5S4ZJML8'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         {children}
         <FeedbackWidget />
       </body>
